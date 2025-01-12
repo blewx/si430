dee374b0175b3876fd8a19447bfffa60c3d47e3e
CoffeeBeann <m261194@usna.edu>
2025-01-12 13:10:44 -0500

    sync

--- labs/lab01/p2.py
+++ labs/lab01/p2.py
@@ -4,19 +4,20 @@ with open(file, 'rb') as f:
     data = f.read()
 
 lower_val = 0
-for i in range(0, len(data)):
+for i in range(len(data)):
     if i % 16 == 0 or i == len(data):
         bytes = data[lower_val:i]
-        print('|',end="")
-        for byte in bytes:
-            if byte == ord('\n'):  #check to see if the byte is = \n, this means
-                                   #that it read in a period as a newline,
-                                   #so change that \n into a . for printing
-                print('.',end="")
-            else:
-                print(chr(byte), end="")
-        print('|')
-        lower_val = i
+        if i > 0:
+            print('|',end="")
+            for byte in bytes:
+                if byte == ord('\n'):  #check to see if the byte is = \n, this means
+                                       #that it read in a period as a newline,
+                                       #so change that \n into a . for printing
+                    print('.',end="")
+                else:
+                    print(chr(byte), end="")
+            print('|')
+            lower_val = i
         print('{:08x}'.format(i), end='  ') #print bytes read sofar
     elif i % 8 == 0:
         print('{:02x}'.format(data[i]), end='  ') #special print to print double
