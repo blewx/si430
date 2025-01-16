0b9c93e20a995646297d183ac775a70787591aaf
CoffeeBeann <m261194@usna.edu>
2025-01-16 09:28:51 -0500

    sycn

---
+++ labs/lab02/serv_udp.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# serv_tcp.py
+
+# Import libraries
+from socket import *
+import sys
+import select
+
+PORT = 9000
+ADDR = "127.0.0.1"
+newaddr = ''
+
+# Open and bind socket
+sock = socket(AF_INET, SOCK_DGRAM)
+sock.bind((ADDR,PORT))
+
+while True:
+    
+    # Get socket list
+    socklist = [sock, sys.stdin]
+    (r_sockets, w_sockets, e_sockets) = select.select(socklist, [], [])
+    
+    # Error
+    if e_sockets:
+        break;
+
+    # Receive Data
+    if sock in r_sockets:
+        data, newaddr = sock.recvfrom(1024)
+        data = data.decode()
+        if not data:
+            break
+        print(data)
+
+    # Write Data
+    if sys.stdin in r_sockets:
+        msg = sys.stdin.readline()
+        if newaddr != '':
+            sock.sendto(msg.encode(), newaddr)
+
+
+sock.close()
