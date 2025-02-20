236da1e800a0649476fcc5eaa79ccc10de62aa3c
BLewiy <m263708@usna.edu>
2025-02-20 08:25:49 -0500

    test

--- labs/lab05/lab05.py
+++ labs/lab05/lab05.py
@@ -1,17 +1,7 @@
 #!bin/usr/python3
 from socket import *
-'''
-ETH_P_ALL = 0x0003
-raw_socket = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))
 
-(data, addr) = raw_socket.recvfrom(1024)
-
-print("addr=", addr)
-print(" ".join([f"{data[i]:02x}" for i in range(16)]))
-print("data=", data)
-'''
-
-def showpkts_TCP():
+def showpkts_UDP():
 
 
     packet_type = [8,0] #udp
@@ -37,13 +27,10 @@ def showpkts_TCP():
         Dest_addr = data[30:34]
         Dest_ip = str(Dest_addr[0]) + "." + str(Dest_addr[1]) + "." + str(Dest_addr[2]) + "." + str(Dest_addr[3])
         Source_ip = str(Source_addr[0]) + "." + str(Source_addr[1]) + "." + str(Source_addr[2]) + "." + str(Source_addr[3])
-        #print(f"{data[12]:02x}", " ", f"{data[13]:02x}")
-        Source_port = data[34] + data[35]*16
-        Dest_port = data[36] + data[37]*16
+        Source_port = data[36]*256 + data[37]
+        Dest_port = data[34]*256 + data[35]
 
         #check to see if it's not an ip packet
-        #print(f"{data[12]:02x}", " ", f"{data[13]:02x}")
-        #print(data[12], " ", f"{data[13]:02x}")
         if data[12] != packet_type[0] or data[13] != packet_type[1]:
             continue
 
@@ -64,97 +51,10 @@ def showpkts_TCP():
                 print("]")
 
 
-        #print("addr=", addr)
-        #print(" ".join([f"{data[i]:02x}" for i in range(18)]))
-        #print("data=", data[14+20:total_length+14].decode())a
-        #
         to_print = ""
         for k in range(20+14+8,total_length+14):
             to_print += chr(data[k])
-            print(chr(data[k]))
         print("",end="   ")
         print(to_print.encode())
 
-
-
-
-
-
-
-
-
-
-
-def showpkts_IP(data):
-
-    data_size = []
-    header = 0
-    offset = []
-    for k in range(24,len(data)):
-        if header == 8: #get size
-            data_size.append(data[k] + data[k+1]*256 + data[k+2]*65536 +
-                    data[k+3]*16777216)
-            offset.append(k+8)
-            header = -(data[k] + data[k+1]*256 + data[k+2]*65536 +
-                    data[k+3]*16777216)- 8
-            #subtract an extra 8 because we're still reading in the data size
-        header += 1          
-
-
-    for i in range(len(data_size)):
-        counter = 1
-        print("Dst-MAC= ", end="")
-        for k in range(offset[i], 6+offset[i]): 
-            print('{:02x}'.format(data[k]),end= "")
-            if k == 5+offset[i]:
-                print('\n',end="")
-            else:
-                print(':',end="")
-
-        #Read in the src-mac
-        print("Src-MAC= ", end="")
-        for k in range(6+offset[i], 12+offset[i]): 
-            print('{:02x}'.format(data[k]),end= "")
-            if k == 11+offset[i]:
-                print('\n',end="")
-            else:
-                print(':',end="")
-
-
-        print("IHL= ", (data[14+offset[i]])%16) #mod 16 bc it is the second bit
-        #not the whole byte
-
-        print("Total Length= ", data[(17+offset[i])])
-
-
-        print("Src-IP= ",end =" ")
-        for k in range(26+offset[i], 30+offset[i]): #read in the src-ip
-            if k == 29+offset[i]:
-                print(data[k],end="\n")
-            else:
-                print(data[k],end=".")
-
-        print("Dst-IP= ",end =" ")
-        for k in range(30+offset[i], 34+offset[i]): #read in the dst-ip
-            if k == 33+offset[i]:
-                print(data[k],end="\n")
-            else:
-                print(data[k],end=".")
-
-
-        print("data:")
-        for k in range(offset[i]+34, offset[i] + 34 + ( data[17+offset[i]] - 20)): #loop through each peice of data to print it
-            print('{:02x}'.format(data[k]),end= " ")
-            if counter  % 16  == 0:
-                print()
-            counter += 1           
-        print()
-
-
-#if '__name__' == '__main__':
-#import sys
-#file = sys.argv[1]
-#with open(file, 'rb') as f:
-#    data = f.read()
-#showpkts_TCP(data, "192.168.172.4", "192.168.172.5")
 showpkts_TCP()
