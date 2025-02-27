365e371abab0179da23904ce216262a120025303
BLewiy <m263708@usna.edu>
2025-02-27 07:50:30 -0500

    finish

--- labs/lab06/p03.py
+++ labs/lab06/p03.py
@@ -24,7 +24,7 @@ s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
 ip_header  = b'\x45\x00\x00\x28'  # Version, IHL, Type of Service | Total Length
 ip_header += b'\xab\xcd\x00\x00'  # Identification | Flags, Fragment Offset
 ip_header += b'\x40\x06\xa6\xec'  # TTL, Protocol | Header Checksum
-ip_header += struct.pack(">BBBB", 192, 168, 172, 4)  # Src IP
+ip_header += struct.pack(">BBBB", 66, 76, 173, 67)  # Source IP (192.168.173.67)
 ip_header += struct.pack(">BBBB", 192, 168, 172, 5)  # Destination IP (192.168.172.5)
 
 #tcp header
@@ -36,7 +36,7 @@ print(tcp_header)
 tcp_header += struct.pack(">HH",  random.randint(1000, 9999), 9000)# Source Port | Destination Port
 tcp_header += struct.pack(">L",  random.randint(1000, 9999)) # Sequence Number
 tcp_header += b'\x50\x02\x71\x10' # Data Offset, Reserved, Flags | Window Size
-srcip = struct.pack(">BBBB", 192, 168, 172, 4)  # Src IP
+srcip = struct.pack(">BBBB", 66, 76, 173, 67)  # Src IP
 dstip =struct.pack(">BBBB", 192, 168, 172, 5)  # Dst IP
 chksum = calc_checksum(srcip + dstip + struct.pack(">BBH", 0, 6, 20) + tcp_header)
 tcp_header += struct.pack(">H", chksum) #calculate checksum based off first 16
